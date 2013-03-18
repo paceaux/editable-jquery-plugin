88e9524b97afd0844336f6a7d97e415c7053e73c
Frank Taylor <paceaux@madebypaceaux.com>
2013-03-18 16:28:59 -0500

    added a fallback for when the browser doesn't support the scoped attr. also added comments

--- editable.plugin.js
+++ editable.plugin.js
@@ -1,21 +1,26 @@
 (function( $ ){
-  $.fn.editable = function( type ) {  
+  $.fn.editable = function( type ) {
+  //SETUP    
       var $this = $(this);
       var $parent = $(this).parent();
-      var stylable = type == 'styles' ? true : false;
+  //INTERNAL FUNCTIONS
       function testFeature (el, attr) {
-              var test = document.createElement(el);
-              if (attr in test) {
-                  return true;
-              } else {
-                  return false;
-              }                
-          }
+        var test = document.createElement(el);
+        if (attr in test) {
+          return true;
+        } else {
+          return false;
+        }                
+      }
+      //set features as a global variable, that way I'm not access it everytime the plugin runs? maybe?    
       var features = {
           contenteditable : testFeature('div', 'contenteditable'),
           scoped : testFeature('style','scoped')
       };
+      var stylable = type == 'styles' ? true : false; // poor practice, need to put all parameters in an object
       console.log(features);
+
+  //PLUGIN METHODS
       var methods = {
           init : function () {
               this.bindEvents();
@@ -23,71 +28,75 @@
               this.makeStylable();
           },
           removeEditable : function (el) {
-              if ($(el).is('[contenteditable="true"]')){
-                  $(el).removeAttr('contenteditable');
-              }     
+            if ($(el).is('[contenteditable="true"]')){
+                $(el).removeAttr('contenteditable');
+            }     
           },
           makeEditable : function (el) {
-                if ( $(el).is('[contenteditable="true"]') ){
-                    $(el).removeAttr('contenteditable');
-                } else {
-                    $(el).attr('contenteditable', true);    
-                }
+            if ( $(el).is('[contenteditable="true"]') ){
+                $(el).removeAttr('contenteditable');
+            } else {
+                $(el).attr('contenteditable', true);    
+            }
           },
           addIndexId : function () {
-              $this.each(function(index) {
-                  $(this).attr('id','ce-'+index);
-              });
+            $this.each(function(index) {
+                $(this).attr('id','ce-'+index);
+            });
           },
           getChildEls: function (el) {
               var childEls = [];
               $(el).children().each(function(index) {
                 var tagName = $(this).prop('tagName');
                   if (index > 0){
-                      //we're avoiding adding the option to style the styles
+                      //don't want to style the style, since it has an index of 0, we get everything after it
                       childEls.push(tagName);
                   }
               });
-              console.log(childEls);
               return childEls;
           },
           addChildElsToStyles: function (p) {
               var childEls = methods.getChildEls($(p));
               $.each(childEls, function(index, value) {
-                  $(p).find('style').append(value+'{}');
+                //where browser doesn't support scope, append the parent ID selector
+                var fallback = features.scoped == true ? '' : "#"+$(p).attr('id')+' ';
+                  $(p).find('style').append(fallback+ value+'{} ');
                 });
           },
           makeStylable : function () {
-              if(stylable == true){
-                  $this.each(function(index) {
-                      var styleBlock = $(this).prepend('<style scoped></style>');
-                  methods.addChildElsToStyles($(this));
-                  });
-              }
+            //need to also add a fallback for feature detection
+            if(stylable == true){
+                $this.each(function(index) {
+                    var styleBlock = $(this).prepend('<style scoped></style>');
+                    methods.addChildElsToStyles($(this));
+                });
+            }
           },
           bindEvents : function () {
-              $this.bind({
-                  dblclick: function() {
-                      methods.makeEditable($(this));
-                      if (stylable == true){
-                          methods.makeEditable($(this).find('style'));
-                      }                      
-                  },
-                  mouseout: function() {
-                      setTimeout(function(){console.log("waiting")},1000);
-                      methods.removeEditable($(this));
-                      if (stylable == true){
-                          methods.removeEditable($(this).find('style'));
-                      }                      
-                  },
-                  focusout: function() {
-                      setTimeout(function(){console.log("waiting")},1000);
-                      methods.removeEditable($(this));
-                      if (stylable == true){
-                          methods.removeEditable($(this).find('style'));
-                      }                                         
-                  }
-                });
+            $this.bind({
+              dblclick: function() {
+                  methods.makeEditable($(this));
+                  if (stylable == true){
+                    //doesn't make sense in my head to do this
+                    //content editable is inherited
+                      methods.makeEditable($(this).find('style'));
+                  }                      
+                },
+              mouseout: function() {
+                setTimeout(function(){console.log("waiting")},1000);
+                methods.removeEditable($(this));
+                  if (stylable == true){
+                      methods.removeEditable($(this).find('style'));
+                  }                      
+                },
+              focusout: function() {
+                setTimeout(function(){console.log("waiting")},1000);
+                methods.removeEditable($(this));
+                if (stylable == true){
+                    methods.removeEditable($(this).find('style'));
+                }                                         
+              }
+            });
           }
     };
      methods.init();
